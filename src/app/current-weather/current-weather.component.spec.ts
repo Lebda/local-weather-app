1d4e0d58ce2ac411d99e89ce27cfcc28bc89781b
jlebduska <j.lebduska@scia.net>
2021-07-19 16:36:40 +0200

    e2e

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,13 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CurrentWeatherComponent } from './current-weather.component';
+import { WeatherService } from '../weather/weather.service';
+import { of } from 'rxjs';
+import { CurrentWeatherMaker } from '../interfaces';
+import { By } from '@angular/platform-browser';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>;
 
   beforeEach(async () => {
+    const weatherServiceSpy = jasmine.createSpyObj('WeatherService', [
+      'getCurrentWeather',
+    ]);
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: WeatherService,
+          useValue: weatherServiceSpy,
+        },
+      ],
       declarations: [CurrentWeatherComponent],
     }).compileComponents();
   });
@@ -15,10 +30,46 @@ describe('CurrentWeatherComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    weatherServiceMock = TestBed.inject(WeatherService) as any;
   });
 
   it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+    // Act
+    fixture.detectChanges(); // triggers ngOnInit
+    // Assert
     expect(component).toBeTruthy();
+  });
+
+  it('should get currentWeather from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+
+    // Act
+    fixture.detectChanges(); // triggers ngOnInit()
+
+    // Assert
+    expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1);
+  });
+
+  it('should eagerly load currentWeather in Bethesda from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(
+      of(CurrentWeatherMaker.Make())
+    );
+
+    // Act
+    fixture.detectChanges(); // triggers ngOnInit()
+
+    // Assert
+    expect(component.current).toBeDefined();
+    expect(component.current.city).toEqual('Bethesda');
+    expect(component.current.temperature).toEqual(72);
+
+    // Assert on DOM
+    const debugEl = fixture.debugElement;
+    const titleEl: HTMLElement = debugEl.query(By.css('span')).nativeElement;
+    expect(titleEl.textContent).toContain('Bethesda');
   });
 });
